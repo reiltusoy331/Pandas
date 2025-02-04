438ef8a4a0e9eafda2041eba06f550856214e8b5
Reil Tusoy <reiltusoy331@gmail.com>
2025-02-04 12:00:37 +0800

    Adding 'Pandas8.py' to my github Pandas repo.

---
+++ Pandas8.py
@@ -0,0 +1,9 @@
+# Name the indexes in a Pandas Series
+
+import pandas as pd
+
+data = [10,20,30,40,50]
+
+dataseries = pd.Series(data,index=["Row1","Row2","Row3","Row4","Row5"])
+
+print("# Pandas Series with custom indexes #\n",dataseries) 
