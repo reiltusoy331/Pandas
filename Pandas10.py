472fbacd64566a4fc4ce1eff027fe7173e030a87
Reil Tusoy <reiltusoy331@gmail.com>
2025-02-05 02:29:53 +0800

    Adding 'Pandas10.py' to my github Pandas repo.

---
+++ Pandas10.py
@@ -0,0 +1,12 @@
+# Pandas Series dtype attribute
+
+import pandas as pd
+
+data = [10,20,30,40,50]
+
+dataseries = pd.Series(data)
+
+print("The Pandas series.\n",dataseries)
+
+print("Pandas \"dtype\" attribute is",dataseries.dtype)
+
