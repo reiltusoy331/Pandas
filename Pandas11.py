cf859b1189b2bfa867fec9afc832f2bbc8e3b938
Reil Tusoy <reiltusoy331@gmail.com>
2025-02-05 02:30:07 +0800

    Adding 'Pandas11.py' to my github Pandas repo.

---
+++ Pandas11.py
@@ -0,0 +1,12 @@
+# Pandas Series ndim attribute
+
+import pandas as pd
+
+data = [10,20,30,40,50]
+
+dataseries = pd.Series(data)
+
+print("The Pandas series.\n",dataseries)
+
+print("Pandas \"ndim\" attribute in this series is",dataseries.ndim)
+
