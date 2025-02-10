f72c9dbd42f0861a75bb694aad8c8f7c0c0647b3
reiltusoy331 <reiltusoy331@gmail.com>
2025-02-10 19:13:31 +0800

    Adding 'Pandas18.py' to my GitHub Pandas repo.

---
+++ Pandas18.py
@@ -0,0 +1,9 @@
+# Read csv file in Pandas
+
+import pandas as pd
+
+# Input the file
+df = pd.read_csv("C:\\Users\\qwerty\\Documents\\Python Pandas\\Students.csv")
+
+#Display the CSV file records
+print("# The Students Dataframe #\n",df)
