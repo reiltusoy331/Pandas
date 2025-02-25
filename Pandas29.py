49ed45b3b5757ae496aa2f0dacd079f14e3c4039
reiltusoy331 <reiltusoy331@gmail.com>
2025-02-26 00:06:59 +0800

    Adding 'Pandas29.py' to my github Pandas repo.

---
+++ Pandas29.py
@@ -0,0 +1,20 @@
+# Pandas deleting a row using drop() method
+
+import pandas as pd
+
+#Dataset 
+Bulls  = {
+    'Player':["Kerr","Jordan","Pippen","Rodman","Longley"],
+    'Jersey':["25","23","33","91","13",],
+    'Drafted Yr':['1988','1983','1987','1981','1990']
+}
+
+Bulls_LineUp = pd.DataFrame(Bulls)
+
+print("Bulls Roster\n",Bulls_LineUp)
+
+# Drop a row\record
+team_roster = Bulls_LineUp.drop(1,axis='index')
+
+# Display the updated DataFrame
+print("\n","Bulls Roster(MJ Retirement)\n",team_roster) 
